51e3307bd1d693533dfd1954fb5549c65209f28a
Michael Kuehl <hello@txs.vc>
2022-04-26 21:48:14 +0200

    parametrized the upload script

--- transaction_simulator/03_upload_transactions.py
+++ transaction_simulator/03_upload_transactions.py
@@ -4,60 +4,104 @@ import os
 import sys
 import json
 import time
-import pandas as pd
+import argparse
 import requests
 import datetime
 
 from simulator.shared import read_from_files
 
-KAFKA_BRIDGE_ROUTE = 'https://transaction-bridge-http-fraud-detection.apps.cluster-4gcwb.4gcwb.sandbox682.opentlc.com'
-KAFKA_TOPIC = 'tx-inbox'
-
-DIR_INPUT = "./simulated-data/tx/"
-
-START_DATE = "2020-04-01"
-END_DATE = "2020-04-30"
-
 KAFKA_HEADERS = {'content-type': 'application/vnd.kafka.json.v2+json'}
-KAFKA_ENDPOINT = f"{KAFKA_BRIDGE_ROUTE}/topics/{KAFKA_TOPIC}"
 
-# read the raw transaction data
-transactions_df = read_from_files(DIR_INPUT, START_DATE, END_DATE)
-NUM_TX = len(transactions_df)
 
-batch = []
-batch_size = 100
+def upload_transactions(args):
+    KAFKA_ENDPOINT = f"{args.bridge}/topics/{args.topic}"
 
-for index, row in transactions_df.iterrows():
-    batch.append(row)
+    # read the raw transaction data
+    transactions_df = read_from_files(args.dir, args.start, args.end)
+    NUM_TX = len(transactions_df)
 
-    if len(batch) % batch_size == 0:
-        payload = {"records": []}
+    batch = []
 
-        for r in batch:
-            record = {'value': r.to_json()}
-            payload['records'].append(record)
+    for index, row in transactions_df.iterrows():
+        batch.append(row)
 
-        try:
-            success = False
-            retry = 0
+        if len(batch) % args.batch_size == 0:
+            payload = {"records": []}
 
-            while not success:
-                r = requests.post(KAFKA_ENDPOINT, headers=KAFKA_HEADERS, json=payload)
-                if r.status_code == 200:
-                    success = True
-                else:
-                    retry = retry + 1
-                    if retry > 5:
-                        print('aborting...')
-                        sys.exit()
-                    time.sleep(retry * 2)
-                    print(f"backing-off/retry {retry}/5")
-        except:
-            print('exception/aborting...')
-            sys.exit()
+            for r in batch:
+                record = {'value': r.to_json()}
+                payload['records'].append(record)
 
-        batch = []
-        print(f"uploaded {index+1}/{NUM_TX}")
+            # post the payload with backoff/retry in case the bridge gets overloaded ...
+            try:
+                success = False
+                retry = 0
 
-print("done.")
+                while not success:
+                    r = requests.post(
+                        KAFKA_ENDPOINT, headers=KAFKA_HEADERS, json=payload)
+                    if r.status_code == 200:
+                        success = True
+                    else:
+                        retry = retry + 1
+                        if retry > 5:
+                            print('aborting...')
+                            sys.exit()
+                        time.sleep(retry * 2)
+                        print(f"backing-off/retry {retry}/5")
+            except:
+                print('exception/aborting...')
+                sys.exit()
+
+            batch = []
+            print(f" --> uploaded {index+1}/{NUM_TX}")
+
+
+def setup():
+
+    parser = argparse.ArgumentParser()
+
+    # kafka bridge endpoint
+    parser.add_argument(
+        '--bridge',
+        required=True
+    )
+
+    # start and end date
+    parser.add_argument(
+        '--start',
+        default='2020-04-01'
+    )
+    parser.add_argument(
+        '--end',
+        default='2020-04-02'
+    )
+
+    # environment
+    parser.add_argument(
+        '--topic',
+        default='tx-inbox'
+    )
+    parser.add_argument(
+        '--dir',
+        default='./simulated-data/pkl/'
+    )
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=100
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    # parse the command line parameters first
+    args = setup()
+
+    print('')
+    print(f" --> Replaying transactions from {args.start} to {args.end}")
+
+    upload_transactions(args)
+
+    print(" --> DONE.")
