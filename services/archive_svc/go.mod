726835e98d139eaa37e6a8166dd674d4342495b2
Michael Kuehl <hello@txs.vc>
2022-04-28 13:23:40 +0200

    added service configuration

--- services/archive_svc/go.mod
+++ services/archive_svc/go.mod
@@ -2,4 +2,7 @@ module github.com/redhat-capgemini-exchange/fsi-fraud-detection/services/archive
 
 go 1.16
 
-require github.com/confluentinc/confluent-kafka-go v1.8.2
+require (
+	github.com/confluentinc/confluent-kafka-go v1.8.2
+	github.com/txsvc/stdlib/v2 v2.1.0
+)
--- services/archive_svc/main.go
+++ services/archive_svc/main.go
@@ -4,21 +4,36 @@ import (
 	"fmt"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/txsvc/stdlib/v2/env"
 )
 
 func main() {
 
+	kafkaService := env.GetString("kafka_service", "")
+	if kafkaService == "" {
+		panic(fmt.Errorf("missing env 'kafka_service'"))
+	}
+	kafkaPort := env.GetString("kafka_port", "9092")
+	kafkaServer := fmt.Sprintf("%s:%s", kafkaService, kafkaPort)
+
+	clientID := env.GetString("client_id", "archive-svc")
+	groupID := env.GetString("group_id", "fsi-fraud-detection")
+
+	source := env.GetString("source_topic", "tx-archive")
+
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost",
-		"group.id":          "myGroup",
-		"auto.offset.reset": "earliest",
+		"bootstrap.servers":   kafkaServer,
+		"client.id":           clientID,
+		"group.id":            groupID,
+		"consumer.timeout.ms": -1,
+		"auto.offset.reset":   "earliest",
 	})
 
 	if err != nil {
 		panic(err)
 	}
 
-	c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
+	c.SubscribeTopics([]string{source}, nil)
 
 	for {
 		msg, err := c.ReadMessage(-1)
