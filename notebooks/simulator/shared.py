97ad97752a423b5b8bee9fe5c74aee91ab2f4cce
Michael Kuehl <hello@txs.vc>
2022-05-17 11:40:07 +0200

    improved the notebooks and data posting

--- notebooks/simulator/shared.py
+++ notebooks/simulator/shared.py
@@ -93,10 +93,38 @@ def load_transactions(file_collection, cutoff_date=None, time_window=-1):
     return td_df
 
 
+def post_to_kafka_bridge(endpoint, batch):
+    KAFKA_HEADERS = {'content-type': 'application/vnd.kafka.json.v2+json'}
+
+    payload = {"records": []}
+    for r in batch:
+        record = {'value': r.to_json()}
+        payload['records'].append(record)
+
+    # post the payload with backoff/retry in case the bridge gets overloaded ...
+    try:
+        success = False
+        retry = 0
+
+        while not success:
+            r = requests.post(endpoint, headers=KAFKA_HEADERS, json=payload)
+            if r.status_code == 200:
+                success = True
+            else:
+                retry = retry + 1
+                if retry > 5:
+                    print('aborting...')
+                    sys.exit()
+                time.sleep(retry * 2)
+                print(f"backing-off/retry {retry}/5")
+    except:
+        print('exception/aborting...')
+        sys.exit()
+
+
 def upload_transactions(bridge, topic='tx-sim', start='2020-05-01', end='2020-05-01', loc='./data/simulated/pkl/', batch_size=100):
 
     KAFKA_ENDPOINT = f"{bridge}/topics/{topic}"
-    KAFKA_HEADERS = {'content-type': 'application/vnd.kafka.json.v2+json'}
 
     # read the raw transaction data
     transactions_df = read_from_pkl(loc, start, end)
@@ -108,41 +136,19 @@ def upload_transactions(bridge, topic='tx-sim', start='2020-05-01', end='2020-05
         batch.append(row)
 
         if len(batch) % batch_size == 0:
-            payload = {"records": []}
-
-            for r in batch:
-                record = {'value': r.to_json()}
-                payload['records'].append(record)
-
-            # post the payload with backoff/retry in case the bridge gets overloaded ...
-            try:
-                success = False
-                retry = 0
-
-                while not success:
-                    r = requests.post(
-                        KAFKA_ENDPOINT, headers=KAFKA_HEADERS, json=payload)
-                    if r.status_code == 200:
-                        success = True
-                    else:
-                        retry = retry + 1
-                        if retry > 5:
-                            print('aborting...')
-                            sys.exit()
-                        time.sleep(retry * 2)
-                        print(f"backing-off/retry {retry}/5")
-            except:
-                print('exception/aborting...')
-                sys.exit()
+            post_to_kafka_bridge(KAFKA_ENDPOINT, batch)
 
             batch = []
             print(f" --> uploaded {index+1}/{NUM_TX}")
+
+    if len(batch) > 0:
+        post_to_kafka_bridge(KAFKA_ENDPOINT, batch)
+        print(f" --> uploaded remaining {len(batch)}/{NUM_TX}")
 
 
 def upload_fraud(bridge, topic='tx-fraud-sim', start='2020-04-01', end='2020-04-01', loc='./data/simulated/fraud/', batch_size=100):
 
     KAFKA_ENDPOINT = f"{bridge}/topics/{topic}"
-    KAFKA_HEADERS = {'content-type': 'application/vnd.kafka.json.v2+json'}
 
     # read the raw transaction data
     transactions_df = read_from_csv(loc, start, end)
@@ -154,35 +160,14 @@ def upload_fraud(bridge, topic='tx-fraud-sim', start='2020-04-01', end='2020-04-
         batch.append(row)
 
         if len(batch) % batch_size == 0:
-            payload = {"records": []}
-
-            for r in batch:
-                record = {'value': r.to_json()}
-                payload['records'].append(record)
-            
-            # post the payload with backoff/retry in case the bridge gets overloaded ...
-            try:
-                success = False
-                retry = 0
-
-                while not success:
-                    r = requests.post(
-                        KAFKA_ENDPOINT, headers=KAFKA_HEADERS, json=payload)
-                    if r.status_code == 200:
-                        success = True
-                    else:
-                        retry = retry + 1
-                        if retry > 5:
-                            print('aborting...')
-                            sys.exit()
-                        time.sleep(retry * 2)
-                        print(f"backing-off/retry {retry}/5")
-            except:
-                print('exception/aborting...')
-                sys.exit()
+            post_to_kafka_bridge(KAFKA_ENDPOINT, batch)
 
             batch = []
             print(f" --> uploaded {index+1}/{NUM_TX}")
+
+    if len(batch) > 0:
+        post_to_kafka_bridge(KAFKA_ENDPOINT, batch)
+        print(f" --> uploaded remaining {len(batch)}/{NUM_TX}")
 
 
 def post_transactions(endpoint, start='2020-05-01', end='2020-05-01', loc='./data/archive/audit/', batch_size=1):
